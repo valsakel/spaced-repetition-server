29dd25e242063456ec396ff9ff9f8143c843f85b
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-07-12 16:28:23 -0500

    Refactor local database

--- config.js
+++ config.js
@@ -4,15 +4,10 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+    process.env.DATABASE_URL || 'mongodb://localhost/flashfluent-backend',
   TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test',
-	JWT_SECRET: process.env.JWT_SECRET,
-	JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
+    process.env.TEST_DATABASE_URL ||
+    'mongodb://localhost/flashfluent-backend-test',
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
